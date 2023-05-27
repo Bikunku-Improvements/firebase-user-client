477be5ab9a56d6e44da4a57d564b657bb2d6a576
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2023-05-27 19:30:56 +0700

    Update BASE_URL

--- components/constant/urls.js
+++ components/constant/urls.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://bikunku-be.fly.dev";
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://bikunku-be.fly.dev";
+// const BASE_URL = "http://localhost:8000";
 const OLD_URL = "api.bikunku.com";
 const URL = "localhost:8000";
 
